abd6ce113ca589ac47d9c4a2d760ae4c6374be24
Denis Mazurov <19bobrik92@gmail.com>
2022-11-30 08:46:49 +0200

    imports

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,7 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
