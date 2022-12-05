34e70abec2a7e602aa5c039b5b74d2a9112ea379
Denis Mazurov <19bobrik92@gmail.com>
2022-12-05 11:46:23 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,14 @@
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const STORAGE_KEY = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
