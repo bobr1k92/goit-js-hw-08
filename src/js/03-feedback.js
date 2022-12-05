02cfcbb9805291e277825e8cfa09a96cebc9bbc7
Denis Mazurov <19bobrik92@gmail.com>
2022-12-05 10:16:17 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import { throttle, update } from "lodash";
 
+const STORAGE_KEY = "feedback-form-state";          
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(updateLocalStorage, 500));
+form.addEventListener('submit', handleSubmit);
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+parseLocalStorage();
+
+function updateLocalStorage(e) {
+    const fieldName = e.target.name;
+    formData[fieldName] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function parseLocalStorage() {
+    const actualData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (actualData) {
+        email.value = actualData.email || '';
+        message.value = actualData.message || '';
+    }
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const {email, message} = (e.currentTarget.elements);
+    console.log({email: email.value, message: message.value});
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
